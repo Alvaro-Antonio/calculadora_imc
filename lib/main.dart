5280ab20b0203e1e703aac19a11539345135d149
Alvaro-Antonio <alvaro.antonio@dce.ufpb.br>
2020-04-28 15:13:00 -0300

    Change app-launcher

--- lib/main.dart
+++ lib/main.dart
@@ -38,18 +38,18 @@ class _HomeState extends State<Home> {
 
       double imc = weight / (height * height);
 
-      if (imc < 18.6 ){
-        _info = "Abaixo do peso {${imc.toStringAsPrecision(3)}}";
-      }else if (imc >= 18.6 && imc < 24.9){
-        _info = "Peso Ideal {${imc.toStringAsPrecision(3)}}";
-      }else if(imc >= 24.9 && imc < 29.9 ){
-        _info = "Levemente acima do peso Ideal {${imc.toStringAsPrecision(3)}}";
-      }else if(imc >=29.9 && imc < 34.9){
-        _info = "Obesidade Grau I {${imc.toStringAsPrecision(3)}}";
-      }else if(imc <= 34.9 && imc < 39.9){
-        _info = "Obesidade Grau II {${imc.toStringAsPrecision(3)}}";
-      }else if(imc >= 40){
-        _info = "Obesidade Grau III {${imc.toStringAsPrecision(3)}}";
+      if(imc < 18.6){
+        _info = "Abaixo do Peso (${imc.toStringAsPrecision(3)})";
+      } else if(imc >= 18.6 && imc < 24.9){
+        _info = "Peso Ideal (${imc.toStringAsPrecision(3)})";
+      } else if(imc >= 24.9 && imc < 29.9){
+        _info = "Levemente Acima do Peso (${imc.toStringAsPrecision(3)})";
+      } else if(imc >= 29.9 && imc < 34.9){
+        _info = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
+      } else if(imc >= 34.9 && imc < 39.9){
+        _info = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
+      } else if(imc >= 40){
+        _info = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
       }
     });
   }
